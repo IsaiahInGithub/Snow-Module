45d949ae07470562a70c8802b54955a3495d1dcc
Isaiah Gomez <89576103+IsaiahfromIndia@users.noreply.github.com>
2022-07-05 14:33:29 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    returner(
+        head(
+            link('style.css'),
+            title('Snow App'),
+        ),
+        body(
+            h1('Hello Snow'),
+        ),
+    );
+}
+
+App();
